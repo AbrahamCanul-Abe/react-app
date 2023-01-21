eab1753c40e6923e61871b6daead00744bb33ecf
Abraham Canul <abraham23.acc@gmail.com>
2023-01-21 15:33:59 -0600

    JSX y fragment

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello World</h1>);
+
+function Greeting() {
+	function add(x, y) {
+		return x + y;
+	}
+	return <h1>{add(10, 30)}</h1>;
+}
+
+root.render(
+	<>
+		<Greeting />
+		<Greeting />
+		<Greeting />
+		<Greeting />
+	</>
+);
